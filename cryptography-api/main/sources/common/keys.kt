2a034d11808aaee458a1b9f73510b647fcb69013
Oleg Yukhnevich <whyoleg@gmail.com>
2022-12-16 01:49:08 +0300

    minor

---
+++ cryptography-api/main/sources/common/keys.kt
@@ -0,0 +1,17 @@
+package dev.whyoleg.cryptography.api
+
+/**
+ * TODO
+ *  - key / keypair / symmetric(secret) key / asymmetric (private + public) key
+ *  - generate key
+ *  - derive key
+ *  - key agreement (?)
+ *  - key exchange (?)
+ *  - keystore/keymanager/keychain/keyring
+ *  - import key
+ *  - export key
+ *  - wrap key
+ *  - unwrap key
+ *  - key formats
+ *  - key usages
+ */
