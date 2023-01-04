adee2286f8ce8049c9a554a0c25be514c6d97656
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-04 09:41:28 +0300

    wip general tests

--- cryptography-tests/cryptography-testsuite-main/test/sources/common/aes.kt
+++ cryptography-tests/cryptography-testsuite-main/test/sources/common/aes.kt
@@ -1,12 +1,71 @@
 package dev.whyoleg.cryptography.testcase.main
 
+import dev.whyoleg.cryptography.algorithms.*
 import dev.whyoleg.cryptography.algorithms.symmetric.*
+import dev.whyoleg.cryptography.provider.*
 import dev.whyoleg.cryptography.random.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.test.*
 import kotlin.random.*
 import kotlin.test.*
+
+interface TestParams
+interface TestData
+interface TestSuite<A : CryptographyAlgorithm, P : TestParams, T : TestData> {
+    suspend fun produce(algorithm: A, params: P): T
+    suspend fun verify(algorithm: A, data: T)
+}
+
+class AesCbcTestParams(
+    val keySize: SymmetricKeySize,
+    val padding: Boolean,
+    val plaintext: ByteArray,
+) : TestParams
+
+class AesCbcTestData(
+    val padding: Boolean,
+    val key: ByteArray,
+    val plaintext: ByteArray,
+    val ciphertext: ByteArray,
+) : TestData
+
+object AesCbcTestSuite : TestSuite<AES.CBC, AesCbcTestParams, AesCbcTestData> {
+    override suspend fun produce(algorithm: AES.CBC, params: AesCbcTestParams): AesCbcTestData {
+        val key = algorithm
+            .keyGenerator(params.keySize)
+            .generateKey()
+
+        val encodedKey = key
+            .encodeTo(AES.Key.Format.RAW)
+
+        assertEquals(params.keySize.value.bytes, encodedKey.size)
+
+        val ciphertext = key
+            .cipher(params.padding)
+            .encrypt(params.plaintext)
+
+        return AesCbcTestData(params.padding, encodedKey, params.plaintext, ciphertext)
+    }
+
+    override suspend fun verify(algorithm: AES.CBC, data: AesCbcTestData) {
+        val plaintext = algorithm
+            .keyDecoder()
+            .decodeFrom(AES.Key.Format.RAW, data.key)
+            .cipher(data.padding)
+            .decrypt(data.ciphertext)
+        assertContentEquals(data.plaintext, plaintext)
+    }
+}
+
+//main
+// - generate data
+// - verify data (locally) (verify)
+// - send data (to remote) (verify)
+
+//cross
+// - receive data (from remote)
+// - verify data (from remote) (verify)
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class AesCbcTest {
@@ -36,7 +95,7 @@ class AesCbcTest {
                             val plaintext = CryptographyRandom.nextBytes(plaintextSize)
                             val ciphertext = cipher.encrypt(plaintext)
 
-                            emit(AesCbcData(true, encodedKey, plaintext, ciphertext))
+                            emit(AesCbcTestData(true, encodedKey, plaintext, ciphertext))
                         }
                     }
                 }
@@ -52,10 +111,3 @@ class AesCbcTest {
         }
     }
 }
-
-public class AesCbcData(
-    public val padding: Boolean,
-    public val key: ByteArray,
-    public val plaintext: ByteArray,
-    public val ciphertext: ByteArray,
-)
