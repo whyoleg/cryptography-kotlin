368ce5073cc6808978f8da69a5ab120314ec24b7
Oleg Yukhnevich <whyoleg@gmail.com>
2023-01-25 23:51:46 +0300

    add build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,18 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    //this version can not be moved to version catalog, because it's in settings.gradle.kts
+    id("com.gradle.enterprise") version "3.12.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 rootProject.name = "cryptography-kotlin"
 
 include("cryptography-io")
