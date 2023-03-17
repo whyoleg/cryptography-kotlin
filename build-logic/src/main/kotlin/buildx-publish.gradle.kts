c886bf7f28d728be9c316d3fc8209d5c4606a3a4
Oleg Yukhnevich <whyoleg@gmail.com>
2023-03-17 16:30:57 +0300

    try to fix flaky error on windows

--- build-logic/src/main/kotlin/buildx-publish.gradle.kts
+++ build-logic/src/main/kotlin/buildx-publish.gradle.kts
@@ -44,34 +44,35 @@ publishing {
             }
         }
     }
+}
 
-    publications.withType<MavenPublication>().configureEach {
+afterEvaluate {
+    val projectDescription = checkNotNull(project.description) { "Project description isn't set for project: ${project.path}" }
+    publishing.publications.withType<MavenPublication>().configureEach {
         artifact(javadocJar)
-        afterEvaluate {
-            pom {
-                name.set(project.name)
-                description.set(checkNotNull(project.description))
-                url.set("https://github.com/whyoleg/cryptography-kotlin")
+        pom {
+            name.set(project.name)
+            description.set(projectDescription)
+            url.set("https://github.com/whyoleg/cryptography-kotlin")
 
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("repo")
-                    }
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
                 }
-                developers {
-                    developer {
-                        id.set("whyoleg")
-                        name.set("Oleg Yukhnevich")
-                        email.set("whyoleg@gmail.com")
-                    }
+            }
+            developers {
+                developer {
+                    id.set("whyoleg")
+                    name.set("Oleg Yukhnevich")
+                    email.set("whyoleg@gmail.com")
                 }
-                scm {
-                    connection.set("https://github.com/whyoleg/cryptography-kotlin.git")
-                    developerConnection.set("https://github.com/whyoleg/cryptography-kotlin.git")
-                    url.set("https://github.com/whyoleg/cryptography-kotlin")
-                }
+            }
+            scm {
+                connection.set("https://github.com/whyoleg/cryptography-kotlin.git")
+                developerConnection.set("https://github.com/whyoleg/cryptography-kotlin.git")
+                url.set("https://github.com/whyoleg/cryptography-kotlin")
             }
         }
     }
