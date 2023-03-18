661f86ef19bee05bcd1448d89d42af687206ed23
Oleg Yukhnevich <whyoleg@gmail.com>
2023-03-19 01:40:36 +0300

    increase JS timeouts

--- build-logic/src/main/kotlin/buildx-target-js.gradle.kts
+++ build-logic/src/main/kotlin/buildx-target-js.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
         nodejs {
             testTask {
                 useMocha {
-                    timeout = "600s"
+                    timeout = "1800s"
                 }
             }
         }
--- gradle/js/karma/config.js
+++ gradle/js/karma/config.js
@@ -4,6 +4,6 @@
 
 config.client = config.client || {}
 config.client.mocha = config.client.mocha || {}
-config.client.mocha.timeout = '600s'
-config.browserNoActivityTimeout = 600000
-config.browserDisconnectTimeout = 600000
+config.client.mocha.timeout = '1800s'
+config.browserNoActivityTimeout = 1800000
+config.browserDisconnectTimeout = 1800000
