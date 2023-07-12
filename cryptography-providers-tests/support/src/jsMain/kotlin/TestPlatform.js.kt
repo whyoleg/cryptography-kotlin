b89356b6d4209cfbae388fbaebd8cb8ebd807189
Oleg Yukhnevich <whyoleg@gmail.com>
2023-07-12 09:31:53 +0300

    fix platform

--- cryptography-providers-tests/support/src/jsMain/kotlin/TestPlatform.js.kt
+++ cryptography-providers-tests/support/src/jsMain/kotlin/TestPlatform.js.kt
@@ -9,15 +9,15 @@ import kotlinx.browser.*
 internal actual val currentTestPlatform: TestPlatform = run {
     when (js("typeof process !== 'undefined' && process.versions != null && process.versions.node != null").unsafeCast<Boolean>()) {
         true  -> TestPlatform.NodeJS(
-            version = js("process.version").unsafeCast<String>(),
-            os = js("process.platform").unsafeCast<String>(),
-            arch = js("process.arch").unsafeCast<String>()
+            version = js("process.version").unsafeCast<String?>() ?: "",
+            os = js("process.platform").unsafeCast<String?>() ?: "",
+            arch = js("process.arch").unsafeCast<String?>() ?: ""
         )
         // https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData
         // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/platform
         false -> TestPlatform.Browser(
-            brand = js("navigator.userAgentData ? navigator.userAgentData.brand : ''").unsafeCast<String>(),
-            platform = js("navigator.userAgentData ? navigator.userAgentData.platform : navigator.platform").unsafeCast<String>(),
+            brand = js("navigator.userAgentData ? navigator.userAgentData.brand : ''").unsafeCast<String?>() ?: "",
+            platform = js("navigator.userAgentData ? navigator.userAgentData.platform : navigator.platform").unsafeCast<String?>() ?: "",
             userAgent = window.navigator.userAgent
         )
     }
