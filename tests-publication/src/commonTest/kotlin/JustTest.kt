cb3362987181b22f125502965178d6c68225aeb0
Oleg Yukhnevich <whyoleg@gmail.com>
2023-09-05 08:57:53 +0300

    update tests-publication with version 0.2.0

--- tests-publication/src/commonTest/kotlin/JustTest.kt
+++ tests-publication/src/commonTest/kotlin/JustTest.kt
@@ -2,8 +2,8 @@
  * Copyright (c) 2023 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.digest.*
-import dev.whyoleg.cryptography.provider.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.*
 import kotlin.test.*
