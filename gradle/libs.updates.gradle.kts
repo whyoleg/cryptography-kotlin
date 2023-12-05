ac9239e1880daf3efe786c02477e00b74da89cd4
Oleg Yukhnevich <whyoleg@gmail.com>
2023-12-05 20:33:54 +0200

    fix updates script

--- gradle/libs.updates.gradle.kts
+++ gradle/libs.updates.gradle.kts
@@ -4,7 +4,7 @@
 
 /**
  * run to check for dependencies:
- *  ./gradlew dependencyUpdates --init-script gradle/libs.updates.gradle.kts
+ *  ./gradlew dependencyUpdates --init-script gradle/libs.updates.gradle.kts --no-configuration-cache
  */
 
 initscript {
