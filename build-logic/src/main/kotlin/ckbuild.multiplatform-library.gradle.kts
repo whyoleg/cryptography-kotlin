335e61bef589f818b2af551b357f223075b8e688
Oleg Yukhnevich <whyoleg@gmail.com>
2024-07-11 19:00:38 +0300

    enable BCV for klibs

--- build-logic/src/main/kotlin/ckbuild.multiplatform-library.gradle.kts
+++ build-logic/src/main/kotlin/ckbuild.multiplatform-library.gradle.kts
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2023 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2023-2024 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
+
+import kotlinx.validation.*
 
 plugins {
     id("ckbuild.multiplatform-base")
@@ -12,4 +14,11 @@ plugins {
 
 kotlin {
     explicitApi()
+}
+
+apiValidation {
+    @OptIn(ExperimentalBCVApi::class)
+    klib {
+        enabled = true
+    }
 }
