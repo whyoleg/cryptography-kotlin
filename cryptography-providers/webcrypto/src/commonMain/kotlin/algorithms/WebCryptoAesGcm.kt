cabd5d29212719cffaa40f9fe440f58ca50f82ca
Lukáš Moravec <morki@morki.cz>
2024-07-28 00:47:39 +0200

    WIP

--- cryptography-providers/webcrypto/src/commonMain/kotlin/algorithms/WebCryptoAesGcm.kt
+++ cryptography-providers/webcrypto/src/commonMain/kotlin/algorithms/WebCryptoAesGcm.kt
@@ -6,7 +6,6 @@ package dev.whyoleg.cryptography.providers.webcrypto.algorithms
 
 import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.symmetric.*
-import dev.whyoleg.cryptography.operations.cipher.*
 import dev.whyoleg.cryptography.providers.webcrypto.internal.*
 import dev.whyoleg.cryptography.providers.webcrypto.materials.*
 import dev.whyoleg.cryptography.random.*
@@ -16,7 +15,7 @@ internal object WebCryptoAesGcm : WebCryptoAes<AES.GCM.Key>(
     keyWrapper = WebCryptoKeyWrapper(arrayOf("encrypt", "decrypt"), ::AesGcmKey)
 ), AES.GCM {
     private class AesGcmKey(key: CryptoKey) : AesKey(key), AES.GCM.Key {
-        override fun cipher(tagSize: BinarySize): AuthenticatedCipher = AesGcmCipher(key, tagSize.inBits)
+        override fun cipher(tagSize: BinarySize): AES.IvAuthenticatedCipher = AesGcmCipher(key, tagSize.inBits)
     }
 }
 
@@ -25,12 +24,16 @@ private const val ivSizeBytes = 12 //bytes for GCM
 private class AesGcmCipher(
     private val key: CryptoKey,
     private val tagSizeBits: Int,
-) : AuthenticatedCipher {
+) : AES.IvAuthenticatedCipher {
 
     override suspend fun encrypt(plaintextInput: ByteArray, associatedData: ByteArray?): ByteArray {
         val iv = CryptographyRandom.nextBytes(ivSizeBytes)
+        return iv + encrypt(iv, plaintextInput, associatedData)
+    }
 
-        val ciphertext = WebCrypto.encrypt(
+    @DelicateCryptographyApi
+    override suspend fun encrypt(iv: ByteArray, plaintextInput: ByteArray, associatedData: ByteArray?): ByteArray {
+        return WebCrypto.encrypt(
             algorithm = AesGcmCipherAlgorithm(
                 additionalData = associatedData,
                 iv = iv,
@@ -39,21 +42,34 @@ private class AesGcmCipher(
             key = key,
             data = plaintextInput
         )
-
-        return iv + ciphertext
     }
 
     override suspend fun decrypt(ciphertextInput: ByteArray, associatedData: ByteArray?): ByteArray {
+        return decrypt(
+            ciphertextInput.copyOfRange(0, ivSizeBytes),
+            ciphertextInput.copyOfRange(ivSizeBytes, ciphertextInput.size),
+            associatedData
+        )
+    }
+
+    @DelicateCryptographyApi
+    override suspend fun decrypt(iv: ByteArray, ciphertextInput: ByteArray, associatedData: ByteArray?): ByteArray {
         return WebCrypto.decrypt(
             algorithm = AesGcmCipherAlgorithm(
                 additionalData = associatedData,
-                iv = ciphertextInput.copyOfRange(0, ivSizeBytes),
+                iv = iv,
                 tagLength = tagSizeBits
             ),
             key = key,
-            data = ciphertextInput.copyOfRange(ivSizeBytes, ciphertextInput.size)
+            data = ciphertextInput
         )
     }
+
+    @DelicateCryptographyApi
+    override fun decryptBlocking(iv: ByteArray, ciphertextInput: ByteArray, associatedData: ByteArray?): ByteArray = nonBlocking()
+
+    @DelicateCryptographyApi
+    override fun encryptBlocking(iv: ByteArray, plaintextInput: ByteArray, associatedData: ByteArray?): ByteArray = nonBlocking()
 
     override fun decryptBlocking(ciphertextInput: ByteArray, associatedData: ByteArray?): ByteArray = nonBlocking()
     override fun encryptBlocking(plaintextInput: ByteArray, associatedData: ByteArray?): ByteArray = nonBlocking()
