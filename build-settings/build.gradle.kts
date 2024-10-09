5fb61a1d14c442063cf984633ba5466f078d496e
Oleg Yukhnevich <whyoleg@gmail.com>
2024-10-09 22:37:48 +0300

    Update Gradle and Develocity versions

--- build-settings/build.gradle.kts
+++ build-settings/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.gradle:develocity-gradle-plugin:3.18")
+    implementation("com.gradle:develocity-gradle-plugin:3.18.1")
     implementation("com.gradle:common-custom-user-data-gradle-plugin:2.0.2")
     implementation("org.gradle.toolchains:foojay-resolver:0.8.0")
 }
