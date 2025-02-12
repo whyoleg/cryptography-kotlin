7367b5a325c1fdce6596a9cbd6cbb7247691ad90
waltkb <68587968+waltkb@users.noreply.github.com>
2025-02-12 21:25:19 +0100

    Add Ed (edwards curve), EdDSA (Ed25519/Ed448) and XDH (X25519/X448) interfaces

---
+++ cryptography-core/src/commonMain/kotlin/algorithms/XDH.kt
@@ -0,0 +1,25 @@
+package algorithms
+
+import dev.whyoleg.cryptography.CryptographyAlgorithmId
+import dev.whyoleg.cryptography.CryptographyProviderApi
+import dev.whyoleg.cryptography.operations.SharedSecretGenerator
+
+@SubclassOptInRequired(CryptographyProviderApi::class)
+public interface XDH : Ed<XDH.PublicKey, XDH.PrivateKey, XDH.KeyPair> {
+    override val id: CryptographyAlgorithmId<XDH> get() = Companion
+
+    public companion object : CryptographyAlgorithmId<XDH>("XDH")
+
+    @SubclassOptInRequired(CryptographyProviderApi::class)
+    public interface KeyPair : Ed.KeyPair<PublicKey, PrivateKey>
+
+    @SubclassOptInRequired(CryptographyProviderApi::class)
+    public interface PublicKey : Ed.PublicKey {
+        public fun sharedSecretGenerator(): SharedSecretGenerator<PrivateKey>
+    }
+
+    @SubclassOptInRequired(CryptographyProviderApi::class)
+    public interface PrivateKey : Ed.PrivateKey {
+        public fun sharedSecretGenerator(): SharedSecretGenerator<PublicKey>
+    }
+}
