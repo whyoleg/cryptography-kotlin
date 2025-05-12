a715446905a0e0b4d336071c39a9efc7f62750a1
Oleg Yukhnevich <whyoleg@gmail.com>
2025-05-12 19:11:10 +0300

    CryptoKit Digest implementation

--- cryptography-providers/base/src/appleMain/kotlin/NSData.kt
+++ cryptography-providers/base/src/appleMain/kotlin/NSData.kt
@@ -12,26 +12,45 @@ private val EmptyNSData = NSData()
 
 @CryptographyProviderApi
 @OptIn(UnsafeNumber::class, ExperimentalForeignApi::class)
+public fun NSData.getIntoByteArray(
+    destination: ByteArray,
+    destinationOffset: Int = 0,
+): Int {
+    val outputSize = length.convert<Int>()
+    if (outputSize == 0) return 0
+    checkBounds(destination.size, destinationOffset, destinationOffset + outputSize)
+
+    destination.usePinned {
+        getBytes(it.addressOf(destinationOffset), length)
+    }
+    return outputSize
+}
+
+@CryptographyProviderApi
+@OptIn(UnsafeNumber::class, ExperimentalForeignApi::class)
 public fun NSData.toByteArray(): ByteArray {
     if (length.convert<Int>() == 0) return EmptyByteArray
 
-    return ByteArray(length.convert()).apply {
-        usePinned {
-            getBytes(it.addressOf(0), length)
-        }
+    return ByteArray(length.convert()).also {
+        getIntoByteArray(it)
     }
 }
 
 @CryptographyProviderApi
 @OptIn(UnsafeNumber::class, ExperimentalForeignApi::class)
-public fun <R> ByteArray.useNSData(block: (NSData) -> R): R {
+public fun <R> ByteArray.useNSData(
+    startIndex: Int = 0,
+    endIndex: Int = size,
+    block: (NSData) -> R,
+): R {
     if (isEmpty()) return block(EmptyNSData)
+    checkBounds(size, startIndex, endIndex)
 
     return usePinned {
         block(
             NSData.dataWithBytesNoCopy(
-                bytes = it.addressOf(0),
-                length = size.convert(),
+                bytes = it.addressOf(startIndex),
+                length = (endIndex - startIndex).convert(),
                 freeWhenDone = false
             )
         )
--- cryptography-providers/cryptokit/src/commonMain/kotlin/CryptoKitCryptographyProvider.kt
+++ cryptography-providers/cryptokit/src/commonMain/kotlin/CryptoKitCryptographyProvider.kt
@@ -8,21 +8,23 @@ import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.*
 import dev.whyoleg.cryptography.providers.cryptokit.algorithms.*
 import dev.whyoleg.cryptography.providers.cryptokit.internal.swiftinterop.*
+import kotlinx.cinterop.*
 
 private val defaultProvider = lazy { CryptoKitCryptographyProvider }
 
 public val CryptographyProvider.Companion.CryptoKit: CryptographyProvider by defaultProvider
 
+@OptIn(UnsafeNumber::class)
 internal object CryptoKitCryptographyProvider : CryptographyProvider() {
     override val name: String get() = "CryptoKit"
 
     @Suppress("UNCHECKED_CAST")
     override fun <A : CryptographyAlgorithm> getOrNull(identifier: CryptographyAlgorithmId<A>): A? = when (identifier) {
-        MD5    -> CryptoKitDigest(MD5, SwiftHash::md5)
-        SHA1   -> CryptoKitDigest(SHA1, SwiftHash::sha1)
-        SHA256 -> CryptoKitDigest(SHA256, SwiftHash::sha256)
-        SHA384 -> CryptoKitDigest(SHA384, SwiftHash::sha384)
-        SHA512 -> CryptoKitDigest(SHA512, SwiftHash::sha512)
+        MD5    -> CryptoKitDigest(MD5, SwiftHash::md5, SwiftHashAlgorithmMd5)
+        SHA1   -> CryptoKitDigest(SHA1, SwiftHash::sha1, SwiftHashAlgorithmSha1)
+        SHA256 -> CryptoKitDigest(SHA256, SwiftHash::sha256, SwiftHashAlgorithmSha256)
+        SHA384 -> CryptoKitDigest(SHA384, SwiftHash::sha384, SwiftHashAlgorithmSha384)
+        SHA512 -> CryptoKitDigest(SHA512, SwiftHash::sha512, SwiftHashAlgorithmSha512)
 //        HMAC    ->
 //        AES.GCM ->
 //        ECDSA   ->
