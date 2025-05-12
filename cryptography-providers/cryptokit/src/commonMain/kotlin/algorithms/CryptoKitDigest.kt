09bb7007fc39e96b3475b28e0943108f2abd1ee1
Oleg Yukhnevich <whyoleg@gmail.com>
2025-05-12 19:11:10 +0300

    CryptoKit Digest implementation

--- cryptography-providers/cryptokit/src/commonMain/kotlin/algorithms/CryptoKitDigest.kt
+++ cryptography-providers/cryptokit/src/commonMain/kotlin/algorithms/CryptoKitDigest.kt
@@ -8,19 +8,59 @@ import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.*
 import dev.whyoleg.cryptography.operations.*
 import dev.whyoleg.cryptography.providers.base.*
+import dev.whyoleg.cryptography.providers.cryptokit.internal.swiftinterop.*
+import kotlinx.cinterop.*
 import platform.Foundation.*
 
+@OptIn(UnsafeNumber::class)
 internal class CryptoKitDigest(
     override val id: CryptographyAlgorithmId<Digest>,
     private val doHash: (NSData) -> NSData,
+    private val algorithm: SwiftHashAlgorithm,
 ) : Digest, Hasher {
     override fun hasher(): Hasher = this
 
     override fun hashBlocking(data: ByteArray): ByteArray {
-        return data.useNSData(doHash).toByteArray()
+        return data.useNSData(block = doHash).toByteArray()
     }
 
-    override fun createHashFunction(): HashFunction {
-        TODO("Not yet implemented")
+    override fun createHashFunction(): HashFunction = CryptoKitHashFunction(algorithm)
+}
+
+@OptIn(UnsafeNumber::class)
+private class CryptoKitHashFunction(
+    private val algorithm: SwiftHashAlgorithm,
+) : HashFunction {
+    private var _function: SwiftHashFunction? = null
+
+    private val function: SwiftHashFunction
+        get() = _function ?: error("Hash function is closed")
+
+    init {
+        reset()
+    }
+
+    override fun hashIntoByteArray(destination: ByteArray, destinationOffset: Int): Int {
+        return function.doFinal().getIntoByteArray(destination, destinationOffset).also {
+            reset()
+        }
+    }
+
+    override fun hashToByteArray(): ByteArray {
+        return function.doFinal().toByteArray().also {
+            reset()
+        }
+    }
+
+    override fun update(source: ByteArray, startIndex: Int, endIndex: Int) {
+        source.useNSData(startIndex, endIndex, function::doUpdate)
+    }
+
+    override fun reset() {
+        _function = SwiftHashFunction(algorithm)
+    }
+
+    override fun close() {
+        _function = null
     }
 }
--- cryptography-providers/cryptokit/src/commonMain/swift/SwiftHash.swift
+++ cryptography-providers/cryptokit/src/commonMain/swift/SwiftHash.swift
@@ -18,3 +18,42 @@ import Foundation
         return Data(SHA512.hash(data: data))
     }
 }
+
+@objc public enum SwiftHashAlgorithm: Int {
+    case md5
+    case sha1
+    case sha256
+    case sha384
+    case sha512
+}
+
+@objc public class SwiftHashFunction: NSObject {
+    private let algorithm: SwiftHashAlgorithm
+    private var function: any HashFunction
+
+    @objc public init(algorithm: SwiftHashAlgorithm) {
+        self.algorithm = algorithm
+        switch algorithm {
+            case .md5:    function = Insecure.MD5()
+            case .sha1:   function = Insecure.SHA1()
+            case .sha256: function = SHA256()
+            case .sha384: function = SHA384()
+            case .sha512: function = SHA512()
+        }
+        super.init()
+    }
+
+    @objc(doUpdate:) public func doUpdate(data: NSData) {
+        function.update(data: data)
+    }
+
+    @objc public func doFinal() -> Data {
+        return switch algorithm {
+            case .md5:    Data((function as! Insecure.MD5).finalize())
+            case .sha1:   Data((function as! Insecure.SHA1).finalize())
+            case .sha256: Data((function as! SHA256).finalize())
+            case .sha384: Data((function as! SHA384).finalize())
+            case .sha512: Data((function as! SHA512).finalize())
+        }
+    }
+}
