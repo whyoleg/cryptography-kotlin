58cc1ac72bcfd57d435b4b4c799037893fbc9adc
Henric Lind <henric.lind@assaabloy.com>
2025-05-16 16:30:16 +0200

    Removed unused model

--- cryptography-providers-tests-api/src/commonMain/kotlin/compatibility/Models.kt
+++ cryptography-providers-tests-api/src/commonMain/kotlin/compatibility/Models.kt
@@ -87,10 +87,3 @@ data class DerivedSecretData(
     val input: ByteStringAsString,
     val secret: ByteStringAsString,
 ) : TestData
-
-@Serializable
-data class CmacData(
-    val key: ByteStringAsString,
-    val salt: ByteStringAsString,
-    val result: ByteStringAsString,
-) : TestData
