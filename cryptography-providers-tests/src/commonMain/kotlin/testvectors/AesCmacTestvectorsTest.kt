b43f5b7ce7e1bd9d97c2cb465368f9e3cb1f951c
Henric Lind <henric.lind@assaabloy.com>
2025-05-19 10:35:51 +0200

    Inlined AesCmacFunction for Openssl3AesCmac instead of having separate Generator and Verifier as operations

--- cryptography-providers-tests/src/commonMain/kotlin/testvectors/AesCmacTestvectorsTest.kt
+++ cryptography-providers-tests/src/commonMain/kotlin/testvectors/AesCmacTestvectorsTest.kt
@@ -13,48 +13,48 @@ import kotlin.test.*
  * https://github.com/aead/cmac/blob/master/vectors_test.go
  * https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/AES_Core_All.pdf
  */
-abstract class AesCmacTestvectorsTest(provider: CryptographyProvider) : AlgorithmTest<AES.CMAC>(AES.CMAC, provider) {
+abstract class AesCmacTestvectorsTest(provider: CryptographyProvider) : AlgorithmTest<CMAC>(CMAC, provider) {
 
-    private fun testCase(key: ByteArray, salt: ByteArray, expected: ByteArray) {
+    private fun testCase(key: String, salt: String, expected: String) {
         testWithAlgorithm {
-            val key = algorithm.keyDecoder().decodeFromByteArrayBlocking(format = Key.Format.RAW, bytes = key)
+            val key = algorithm.keyDecoder().decodeFromByteArrayBlocking(format = Key.Format.RAW, bytes = key.hexToByteArray())
             val result = key.signatureGenerator().createSignFunction()
-                .apply { update(salt) }
+                .apply { update(salt.hexToByteArray()) }
                 .signToByteArray()
             assertEquals(16, result.size)
-            assertEquals(result.toHexString(), expected.toHexString())
+            assertEquals(result.toHexString(), expected)
         }
     }
 
     @Test
     fun testDiversifyKeyCase1() {
-        val key = "2b7e151628aed2a6abf7158809cf4f3c".hexToByteArray()
-        val salt = "6bc1bee22e409f96e93d7e117393172a".hexToByteArray()
-        val result = "070a16b46b4d4144f79bdd9dd04a287c".hexToByteArray()
+        val key = "2b7e151628aed2a6abf7158809cf4f3c"
+        val salt = "6bc1bee22e409f96e93d7e117393172a"
+        val result = "070a16b46b4d4144f79bdd9dd04a287c"
         testCase(key, salt, result)
     }
 
     @Test
     fun testDiversifyKeyCase2() {
-        val key = "2b7e151628aed2a6abf7158809cf4f3c".hexToByteArray()
-        val salt = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411".hexToByteArray()
-        val result = "dfa66747de9ae63030ca32611497c827".hexToByteArray()
+        val key = "2b7e151628aed2a6abf7158809cf4f3c"
+        val salt = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411"
+        val result = "dfa66747de9ae63030ca32611497c827"
         testCase(key, salt, result)
     }
 
     @Test
     fun testDiversifyKeyCase3() {
-        val key = "2b7e151628aed2a6abf7158809cf4f3c".hexToByteArray()
-        val salt = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411".hexToByteArray()
-        val result = "dfa66747de9ae63030ca32611497c827".hexToByteArray()
+        val key = "2b7e151628aed2a6abf7158809cf4f3c"
+        val salt = "6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411"
+        val result = "dfa66747de9ae63030ca32611497c827"
         testCase(key, salt, result)
     }
 
     @Test
     fun testDiversifyKeyCase4() {
-        val key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4".hexToByteArray()
-        val salt = "".hexToByteArray()
-        val result = "028962f61b7bf89efc6b551f4667d983".hexToByteArray()
+        val key = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"
+        val salt = ""
+        val result = "028962f61b7bf89efc6b551f4667d983"
         testCase(key, salt, result)
     }
 }
--- cryptography-providers/openssl3/api/src/commonMain/kotlin/algorithms/Openssl3AesCmac.kt
+++ cryptography-providers/openssl3/api/src/commonMain/kotlin/algorithms/Openssl3AesCmac.kt
@@ -7,7 +7,9 @@ package dev.whyoleg.cryptography.providers.openssl3.algorithms
 import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.*
 import dev.whyoleg.cryptography.operations.*
-import dev.whyoleg.cryptography.providers.openssl3.operations.*
+import dev.whyoleg.cryptography.providers.base.*
+import dev.whyoleg.cryptography.providers.openssl3.internal.*
+import dev.whyoleg.cryptography.providers.openssl3.internal.cinterop.*
 import kotlinx.cinterop.*
 import kotlin.experimental.*
 
@@ -22,14 +24,9 @@ internal object Openssl3AesCmac : AES.CMAC, Openssl3Aes<AES.CMAC.Key>() {
             AES.Key.Size.B256 -> "AES-256-CBC"
             else              -> error("Unsupported key size")
         }
-
-        private fun createFunction() = AesCmacSignature(
-            algorithm = algorithm,
-            key = key
-        )
-
-        override fun signatureGenerator(): SignatureGenerator = createFunction()
-        override fun signatureVerifier(): SignatureVerifier = createFunction()
+        private val signature = AesCmacSignature(algorithm = algorithm, key = key)
+        override fun signatureGenerator(): SignatureGenerator = signature
+        override fun signatureVerifier(): SignatureVerifier = signature
     }
 }
 
@@ -39,18 +36,118 @@ private class AesCmacSignature(
     private val algorithm: String,
     private val key: ByteArray,
 ) : SignatureGenerator, SignatureVerifier {
+    private val mac = checkError(EVP_MAC_fetch(null, "CMAC", null))
+    private fun createFunction() = AesCmacFunction(
+        key = key,
+        algorithm = algorithm,
+        context = Resource(checkError(EVP_MAC_CTX_new(mac)), ::EVP_MAC_CTX_free)
+    )
 
-    override fun createSignFunction(): SignFunction {
-        return Openssl3CmacSignatureGenerator(
-            key = key,
-            algorithm = algorithm
-        ).createSignFunction()
-    }
+    override fun createSignFunction(): SignFunction = createFunction()
+    override fun createVerifyFunction(): VerifyFunction = createFunction()
 
-    override fun createVerifyFunction(): VerifyFunction {
-        return Openssl3CmacSignatureVerifier(
-            key = key,
-            algorithm = algorithm
-        ).createVerifyFunction()
+    private class AesCmacFunction(
+        private val key: ByteArray,
+        private val algorithm: String,
+        private val context: Resource<CPointer<EVP_MAC_CTX>>,
+    ) : SignFunction, VerifyFunction, SafeCloseable(SafeCloseAction(context, AutoCloseable::close)) {
+
+        init {
+            reset()
+        }
+
+        @OptIn(UnsafeNumber::class)
+        override fun update(source: ByteArray, startIndex: Int, endIndex: Int) {
+            // Implementation for updating the CMAC with the provided data
+            checkBounds(source.size, startIndex, endIndex)
+            val context = context.access()
+            source.usePinned {
+                checkError(
+                    EVP_MAC_update(
+                        ctx = context,
+                        data = it.safeAddressOf(0).reinterpret(),
+                        datalen = source.size.convert()
+                    )
+                )
+            }
+        }
+
+        override fun signIntoByteArray(destination: ByteArray, destinationOffset: Int): Int {
+            val signature = signToByteArray()
+            checkBounds(destination.size, destinationOffset, destinationOffset + signature.size)
+            signature.copyInto(destination, destinationOffset, destinationOffset)
+            return signature.size
+        }
+
+        @OptIn(UnsafeNumber::class)
+        override fun signToByteArray(): ByteArray {
+            val context = context.access()
+            val macSize = EVP_MAC_CTX_get_mac_size(context).convert<Int>()
+            val out = ByteArray(macSize)
+            out.usePinned {
+                checkError(
+                    EVP_MAC_final(
+                        ctx = context,
+                        out = it.safeAddressOf(0).reinterpret(),
+                        outl = null,
+                        outsize = macSize.convert()
+                    )
+                )
+            }
+            return out
+        }
+
+        @OptIn(UnsafeNumber::class)
+        override fun reset() {
+            val context = context.access()
+            memScoped {
+                val params = allocArrayOf(
+                    OSSL_PARAM_construct_utf8_string(
+                        "cipher".cstr.ptr,
+                        algorithm.cstr.ptr,
+                        0.convert()
+                    ),
+                    OSSL_PARAM_construct_end()
+                )
+
+                key.usePinned {
+                    checkError(
+                        EVP_MAC_init(
+                            ctx = context,
+                            key = it.addressOf(0).reinterpret(),
+                            keylen = key.size.convert(),
+                            params = params
+                        )
+                    )
+                }
+            }
+        }
+
+        @OptIn(UnsafeNumber::class)
+        override fun tryVerify(signature: ByteArray, startIndex: Int, endIndex: Int): Boolean {
+            checkBounds(signature.size, startIndex, endIndex)
+
+            val context = context.access()
+            val computedMac = ByteArray(EVP_MAC_CTX_get_mac_size(context).convert<Int>())
+
+            computedMac.usePinned { pinnedMac ->
+                checkError(
+                    EVP_MAC_final(
+                        ctx = context,
+                        out = pinnedMac.addressOf(0).reinterpret(),
+                        outl = null,
+                        outsize = computedMac.size.convert()
+                    )
+                )
+            }
+
+            val isValid = computedMac.contentEquals(signature.copyOfRange(startIndex, endIndex))
+            if (!isValid) throw IllegalArgumentException("Invalid signature")
+            return isValid
+        }
+
+        override fun verify(signature: ByteArray, startIndex: Int, endIndex: Int) {
+            check(tryVerify(signature, startIndex, endIndex)) { "Invalid signature" }
+        }
     }
 }
