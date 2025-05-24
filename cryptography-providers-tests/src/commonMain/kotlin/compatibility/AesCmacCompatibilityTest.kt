ef5f0bcb0db8ac1b70198c6d2a95ba5582f623c8
Oleg Yukhnevich <whyoleg@gmail.com>
2025-05-24 07:19:55 +0300

    Add missing copyright headers

--- cryptography-providers-tests/src/commonMain/kotlin/compatibility/AesCmacCompatibilityTest.kt
+++ cryptography-providers-tests/src/commonMain/kotlin/compatibility/AesCmacCompatibilityTest.kt
@@ -1,13 +1,15 @@
+/*
+ * Copyright (c) 2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.whyoleg.cryptography.providers.tests.compatibility
 
 import dev.whyoleg.cryptography.*
 import dev.whyoleg.cryptography.algorithms.*
-import dev.whyoleg.cryptography.providers.tests.api.assertVerifySignature
+import dev.whyoleg.cryptography.providers.tests.api.*
 import dev.whyoleg.cryptography.providers.tests.api.compatibility.*
 import dev.whyoleg.cryptography.random.*
 import kotlinx.io.bytestring.*
-import kotlinx.serialization.*
-import kotlin.test.*
 
 private const val maxPlaintextSize = 10000
 
--- cryptography-providers-tests/src/commonMain/kotlin/testvectors/AesCmacTestvectorsTest.kt
+++ cryptography-providers-tests/src/commonMain/kotlin/testvectors/AesCmacTestvectorsTest.kt
@@ -1,9 +1,11 @@
+/*
+ * Copyright (c) 2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.whyoleg.cryptography.providers.tests.testvectors
 
 import dev.whyoleg.cryptography.*
-import dev.whyoleg.cryptography.algorithms.*
 import dev.whyoleg.cryptography.algorithms.AES.*
-
 import dev.whyoleg.cryptography.providers.tests.api.*
 import kotlin.test.*
 
