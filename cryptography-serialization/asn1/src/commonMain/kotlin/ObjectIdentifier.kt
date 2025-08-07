2e9f0f5c95c560cb26d4a4ab4b09f0bbb1aca8cc
Oleg Yukhnevich <whyoleg@gmail.com>
2025-08-07 21:23:35 +0300

    asn1: new identifier

--- cryptography-serialization/asn1/src/commonMain/kotlin/ObjectIdentifier.kt
+++ cryptography-serialization/asn1/src/commonMain/kotlin/ObjectIdentifier.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2024-2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.whyoleg.cryptography.serialization.asn1
@@ -11,4 +11,25 @@ import kotlin.jvm.*
 @JvmInline
 public value class ObjectIdentifier(public val value: String) {
     public companion object
+}
+
+@Serializable // TODO: what should be the default way? asn.1 bytearray? string?
+@JvmInline
+public class ObjectIdentifier2 private constructor(
+
+) {
+    public val nodes: IntArray get() = TODO()
+    public fun toByteArray(): ByteArray = TODO()
+
+    override fun toString(): String = TODO()
+
+    public companion object {
+        // TODO: have a cache here?
+
+        // 1.1.1.1.1
+        public fun parse(value: String): ObjectIdentifier2 = TODO()
+
+        public fun fromByteArray(bytes: ByteArray): ObjectIdentifier2 = TODO()
+        public fun fromNodes(vararg nodes: Int): ObjectIdentifier2 = TODO()
+    }
 }
