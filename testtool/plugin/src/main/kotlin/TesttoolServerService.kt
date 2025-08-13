5c98c760072bfd7f521bdc446f35f017eb7769c1
Oleg Yukhnevich <whyoleg@gmail.com>
2025-08-14 00:30:13 +0300

    [TT] drop `instanceId` from server

--- testtool/plugin/src/main/kotlin/TesttoolServerService.kt
+++ testtool/plugin/src/main/kotlin/TesttoolServerService.kt
@@ -6,20 +6,15 @@ package dev.whyoleg.cryptography.testtool.plugin
 
 import dev.whyoleg.cryptography.testtool.server.*
 import org.gradle.api.file.*
-import org.gradle.api.provider.*
 import org.gradle.api.services.*
-import java.util.*
 
 abstract class TesttoolServerService : BuildService<TesttoolServerService.Parameters>, AutoCloseable {
     interface Parameters : BuildServiceParameters {
-        val instanceId: Property<String>
-        val storage: DirectoryProperty
+        val storageDirectory: DirectoryProperty
     }
 
-    // use random prefix to easy local dev
     private val server = startTesttoolServer(
-        instanceId = "${parameters.instanceId.get()}-${UUID.randomUUID()}",
-        storagePath = parameters.storage.get().asFile.toPath()
+        storagePath = parameters.storageDirectory.get().asFile.toPath()
     )
 
     override fun close(): Unit = server.close()
