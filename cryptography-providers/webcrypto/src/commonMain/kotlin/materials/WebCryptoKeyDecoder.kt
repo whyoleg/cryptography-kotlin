36969fa89297592aa653910f2965f309110313b6
Oleg Yukhnevich <whyoleg@gmail.com>
2025-09-18 22:00:15 +0300

    WIP: tests

--- cryptography-providers/webcrypto/src/commonMain/kotlin/materials/WebCryptoKeyDecoder.kt
+++ cryptography-providers/webcrypto/src/commonMain/kotlin/materials/WebCryptoKeyDecoder.kt
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2023-2024 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2023-2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.whyoleg.cryptography.providers.webcrypto.materials
 
 import dev.whyoleg.cryptography.materials.key.*
 import dev.whyoleg.cryptography.providers.webcrypto.internal.*
+import kotlin.io.encoding.*
 
 internal class WebCryptoKeyDecoder<KF : KeyFormat, K : Key>(
     private val algorithm: Algorithm,
@@ -14,7 +15,9 @@ internal class WebCryptoKeyDecoder<KF : KeyFormat, K : Key>(
 ) : KeyDecoder<KF, K> {
     override suspend fun decodeFromByteArray(format: KF, bytes: ByteArray): K = keyWrapper.wrap(
         WebCrypto.importKey(
-            format = keyProcessor.stringFormat(format),
+            format = keyProcessor.stringFormat(format).also {
+                println("format: $it | ${algorithm} | ${Base64.encode(bytes)}\n")
+            },
             keyData = keyProcessor.beforeDecoding(algorithm, format, bytes),
             algorithm = algorithm,
             extractable = true,
