3fb53ae44009df372c89c0fb5ad8fbdffc53c3ce
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-10 21:46:46 +0200

    [base] Extract `AccumulatingSignFunction` and `AccumulatingVerifyFunction` from `apple` provider

--- cryptography-providers/apple/src/commonMain/kotlin/internal/SecSignature.kt
+++ cryptography-providers/apple/src/commonMain/kotlin/internal/SecSignature.kt
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2024 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2024-2026 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.whyoleg.cryptography.providers.apple.internal
 
 import dev.whyoleg.cryptography.operations.*
 import dev.whyoleg.cryptography.providers.base.*
+import dev.whyoleg.cryptography.providers.base.operations.*
 import kotlinx.cinterop.*
 import platform.CoreFoundation.*
 import platform.Foundation.*
@@ -15,48 +16,9 @@ internal class SecSignatureVerifier(
     private val publicKey: SecKeyRef,
     private val algorithm: SecKeyAlgorithm?,
 ) : SignatureVerifier {
-    override fun createVerifyFunction(): VerifyFunction = SecVerifyFunction(publicKey, algorithm)
-}
-
-internal class SecSignatureGenerator(
-    private val privateKey: SecKeyRef,
-    private val algorithm: SecKeyAlgorithm?,
-) : SignatureGenerator {
-    override fun createSignFunction(): SignFunction = SecSignFunction(privateKey, algorithm)
-}
-
-private class SecVerifyFunction(
-    private val publicKey: SecKeyRef,
-    private val algorithm: SecKeyAlgorithm?,
-) : VerifyFunction {
-    private var isClosed = false
-    private var accumulator = EmptyByteArray
-
-    private fun ensureNotClosed() {
-        check(!isClosed) { "Already closed" }
-    }
-
-    override fun update(source: ByteArray, startIndex: Int, endIndex: Int) {
-        ensureNotClosed()
-        checkBounds(source.size, startIndex, endIndex)
-
-        accumulator += source.copyOfRange(startIndex, endIndex)
-    }
-
-    override fun tryVerify(signature: ByteArray, startIndex: Int, endIndex: Int): Boolean {
-        return verifyError(signature, startIndex, endIndex) == null
-    }
-
-    override fun verify(signature: ByteArray, startIndex: Int, endIndex: Int) {
-        val error = verifyError(signature, startIndex, endIndex) ?: return
-        error("Invalid signature: $error")
-    }
-
-    private fun verifyError(signature: ByteArray, startIndex: Int, endIndex: Int): String? = memScoped {
-        ensureNotClosed()
-        checkBounds(signature.size, startIndex, endIndex)
+    private fun verify(data: ByteArray, signature: ByteArray): String? = memScoped {
         val error = alloc<CFErrorRefVar>()
-        accumulator.useNSData { data ->
+        data.useNSData { data ->
             signature.useNSData { signature ->
                 val result = SecKeyVerifySignature(
                     key = publicKey,
@@ -70,51 +32,19 @@ private class SecVerifyFunction(
                     else   -> error.value.releaseBridgeAs<NSError>()?.description ?: ""
                 }
             }
-        }.also {
-            reset()
         }
     }
 
-    override fun reset() {
-        ensureNotClosed()
-        accumulator = EmptyByteArray
-    }
-
-    override fun close() {
-        isClosed = true
-        accumulator = EmptyByteArray
-    }
+    override fun createVerifyFunction(): VerifyFunction = AccumulatingVerifyFunction(::verify)
 }
 
-private class SecSignFunction(
+internal class SecSignatureGenerator(
     private val privateKey: SecKeyRef,
     private val algorithm: SecKeyAlgorithm?,
-) : SignFunction {
-    private var isClosed = false
-    private var accumulator = EmptyByteArray
-
-    private fun ensureNotClosed() {
-        check(!isClosed) { "Already closed" }
-    }
-
-    override fun update(source: ByteArray, startIndex: Int, endIndex: Int) {
-        ensureNotClosed()
-        checkBounds(source.size, startIndex, endIndex)
-
-        accumulator += source.copyOfRange(startIndex, endIndex)
-    }
-
-    override fun signIntoByteArray(destination: ByteArray, destinationOffset: Int): Int {
-        val signature = signToByteArray()
-        checkBounds(destination.size, destinationOffset, destinationOffset + signature.size)
-        signature.copyInto(destination, destinationOffset, destinationOffset)
-        return signature.size
-    }
-
-    override fun signToByteArray(): ByteArray = memScoped {
-        ensureNotClosed()
+) : SignatureGenerator {
+    private fun sign(data: ByteArray): ByteArray = memScoped {
         val error = alloc<CFErrorRefVar>()
-        accumulator.useNSData { data ->
+        data.useNSData { data ->
             val signature = SecKeyCreateSignature(
                 key = privateKey,
                 algorithm = algorithm,
@@ -128,18 +58,8 @@ private class SecSignFunction(
             }
 
             signature.toByteArray()
-        }.also {
-            reset()
         }
     }
 
-    override fun reset() {
-        ensureNotClosed()
-        accumulator = EmptyByteArray
-    }
-
-    override fun close() {
-        isClosed = true
-        accumulator = EmptyByteArray
-    }
+    override fun createSignFunction(): SignFunction = AccumulatingSignFunction(::sign)
 }
