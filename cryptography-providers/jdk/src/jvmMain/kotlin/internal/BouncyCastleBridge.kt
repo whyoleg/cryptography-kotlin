e04d19fcc3822538a4b01882e958942e418209db
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-11 09:23:53 +0200

    jdk

--- cryptography-providers/jdk/src/jvmMain/kotlin/internal/BouncyCastleBridge.kt
+++ cryptography-providers/jdk/src/jvmMain/kotlin/internal/BouncyCastleBridge.kt
@@ -1,9 +1,12 @@
 /*
- * Copyright (c) 2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2025-2026 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.whyoleg.cryptography.providers.jdk.internal
 
+import dev.whyoleg.cryptography.providers.jdk.*
+import org.bouncycastle.crypto.params.*
+import org.bouncycastle.jcajce.provider.asymmetric.util.*
 import org.bouncycastle.jce.*
 import java.security.interfaces.*
 import java.security.spec.*
@@ -34,4 +37,57 @@ internal object BouncyCastleBridge {
         return ECPublicKeySpec(point, privateKey.params)
     }
 
+    fun deriveEdDSAPublicKeySpec(
+        privateKey: JPrivateKey,
+    ): KeySpec? {
+        if (!isAvailable) return null
+
+        return try {
+            val bcKey = ECUtil.generatePrivateKeyParameter(privateKey)
+            when (bcKey) {
+                is Ed25519PrivateKeyParameters -> {
+                    val spec = Class.forName("java.security.spec.EdECPublicKeySpec")
+                    val paramSpec = Class.forName("java.security.spec.EdDSAParameterSpec")
+                    val ed25519 = paramSpec.getField("Ed25519").get(null)
+                    spec.getConstructor(paramSpec, ByteArray::class.java).newInstance(ed25519, bcKey.generatePublicKey().encoded) as KeySpec
+                }
+                is Ed448PrivateKeyParameters   -> {
+                    val spec = Class.forName("java.security.spec.EdECPublicKeySpec")
+                    val paramSpec = Class.forName("java.security.spec.EdDSAParameterSpec")
+                    val ed448 = paramSpec.getField("Ed448").get(null)
+                    spec.getConstructor(paramSpec, ByteArray::class.java).newInstance(ed448, bcKey.generatePublicKey().encoded) as KeySpec
+                }
+                else                           -> null
+            }
+        } catch (_: Throwable) {
+            null
+        }
+    }
+
+    fun deriveXDHPublicKeySpec(
+        privateKey: JPrivateKey,
+    ): KeySpec? {
+        if (!isAvailable) return null
+
+        return try {
+            val bcKey = ECUtil.generatePrivateKeyParameter(privateKey)
+            when (bcKey) {
+                is X25519PrivateKeyParameters -> {
+                    val spec = Class.forName("java.security.spec.XECPublicKeySpec")
+                    val paramSpec = Class.forName("java.security.spec.NamedParameterSpec")
+                    val x25519 = paramSpec.getField("X25519").get(null)
+                    spec.getConstructor(paramSpec, ByteArray::class.java).newInstance(x25519, bcKey.generatePublicKey().encoded) as KeySpec
+                }
+                is X448PrivateKeyParameters   -> {
+                    val spec = Class.forName("java.security.spec.XECPublicKeySpec")
+                    val paramSpec = Class.forName("java.security.spec.NamedParameterSpec")
+                    val x448 = paramSpec.getField("X448").get(null)
+                    spec.getConstructor(paramSpec, ByteArray::class.java).newInstance(x448, bcKey.generatePublicKey().encoded) as KeySpec
+                }
+                else                          -> null
+            }
+        } catch (_: Throwable) {
+            null
+        }
+    }
 }
