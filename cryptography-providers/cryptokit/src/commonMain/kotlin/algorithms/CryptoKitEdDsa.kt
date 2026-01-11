7a8e41deb6af0ac5cdc144cb621379e734d490ae
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-11 09:23:30 +0200

    cryptokit

---
+++ cryptography-providers/cryptokit/src/commonMain/kotlin/algorithms/CryptoKitEdDsa.kt
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2025-2026 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.whyoleg.cryptography.providers.cryptokit.algorithms
+
+import dev.whyoleg.cryptography.algorithms.*
+import dev.whyoleg.cryptography.materials.key.*
+import dev.whyoleg.cryptography.operations.*
+import dev.whyoleg.cryptography.providers.base.*
+import dev.whyoleg.cryptography.providers.base.materials.*
+import dev.whyoleg.cryptography.providers.base.operations.*
+import dev.whyoleg.cryptography.providers.cryptokit.internal.*
+import dev.whyoleg.cryptography.providers.cryptokit.internal.swift.DwcCryptoKitInterop.*
+import dev.whyoleg.cryptography.serialization.asn1.*
+import dev.whyoleg.cryptography.serialization.asn1.modules.*
+import dev.whyoleg.cryptography.serialization.pem.*
+
+internal object CryptoKitEdDsa : EdDSA {
+    override fun publicKeyDecoder(curve: EdDSA.Curve): KeyDecoder<EdDSA.PublicKey.Format, EdDSA.PublicKey> {
+        check(curve == EdDSA.Curve.Ed25519) { "CryptoKit supports Ed25519 only" }
+        return PublicKeyDecoder
+    }
+
+    override fun privateKeyDecoder(curve: EdDSA.Curve): KeyDecoder<EdDSA.PrivateKey.Format, EdDSA.PrivateKey> {
+        check(curve == EdDSA.Curve.Ed25519) { "CryptoKit supports Ed25519 only" }
+        return PrivateKeyDecoder
+    }
+
+    override fun keyPairGenerator(curve: EdDSA.Curve): KeyGenerator<EdDSA.KeyPair> {
+        check(curve == EdDSA.Curve.Ed25519) { "CryptoKit supports Ed25519 only" }
+        return KeyPairGenerator
+    }
+
+    private object KeyPairGenerator : KeyGenerator<EdDSA.KeyPair> {
+        override fun generateKeyBlocking(): EdDSA.KeyPair {
+            val privateKey = SwiftEdDsaPrivateKey.generate()
+            return EdDsaKeyPair(
+                publicKey = EdPublicKey(privateKey.publicKey()),
+                privateKey = EdPrivateKey(privateKey)
+            )
+        }
+    }
+
+    private object PublicKeyDecoder : KeyDecoder<EdDSA.PublicKey.Format, EdDSA.PublicKey> {
+        override fun decodeFromByteArrayBlocking(format: EdDSA.PublicKey.Format, bytes: ByteArray): EdDSA.PublicKey = when (format) {
+            EdDSA.PublicKey.Format.RAW -> EdPublicKey(
+                swiftTry { error -> bytes.useNSData { SwiftEdDsaPublicKey.decodeRawWithRawRepresentation(it, error) } }
+            )
+            EdDSA.PublicKey.Format.DER -> {
+                val raw = unwrapSubjectPublicKeyInfo(ObjectIdentifier.Ed25519, bytes)
+                EdPublicKey(swiftTry { error -> raw.useNSData { SwiftEdDsaPublicKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            EdDSA.PublicKey.Format.PEM -> {
+                val der = unwrapPem(PemLabel.PublicKey, bytes)
+                val raw = unwrapSubjectPublicKeyInfo(ObjectIdentifier.Ed25519, der)
+                EdPublicKey(swiftTry { error -> raw.useNSData { SwiftEdDsaPublicKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            else                       -> error("$format is not supported by CryptoKit EdDSA")
+        }
+    }
+
+    private object PrivateKeyDecoder : KeyDecoder<EdDSA.PrivateKey.Format, EdDSA.PrivateKey> {
+        override fun decodeFromByteArrayBlocking(format: EdDSA.PrivateKey.Format, bytes: ByteArray): EdDSA.PrivateKey = when (format) {
+            EdDSA.PrivateKey.Format.RAW -> EdPrivateKey(
+                swiftTry { error -> bytes.useNSData { SwiftEdDsaPrivateKey.decodeRawWithRawRepresentation(it, error) } }
+            )
+            EdDSA.PrivateKey.Format.DER -> {
+                val raw = KeyInfoUnwrap.unwrapPkcs8ForOids(bytes, listOf(ObjectIdentifier.Ed25519))
+                EdPrivateKey(swiftTry { error -> raw.useNSData { SwiftEdDsaPrivateKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            EdDSA.PrivateKey.Format.PEM -> {
+                val der = unwrapPem(PemLabel.PrivateKey, bytes)
+                val raw = KeyInfoUnwrap.unwrapPkcs8ForOids(der, listOf(ObjectIdentifier.Ed25519))
+                EdPrivateKey(swiftTry { error -> raw.useNSData { SwiftEdDsaPrivateKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            else                        -> error("$format is not supported by CryptoKit EdDSA")
+        }
+    }
+
+    private class EdDsaKeyPair(
+        override val publicKey: EdDSA.PublicKey,
+        override val privateKey: EdDSA.PrivateKey,
+    ) : EdDSA.KeyPair
+
+    private class EdPublicKey(
+        val key: SwiftEdDsaPublicKey,
+    ) : EdDSA.PublicKey {
+        override fun encodeToByteArrayBlocking(format: EdDSA.PublicKey.Format): ByteArray = when (format) {
+            EdDSA.PublicKey.Format.RAW -> key.rawRepresentation().toByteArray()
+            EdDSA.PublicKey.Format.DER -> wrapSubjectPublicKeyInfo(
+                UnknownKeyAlgorithmIdentifier(ObjectIdentifier.Ed25519),
+                key.rawRepresentation().toByteArray()
+            )
+            EdDSA.PublicKey.Format.PEM -> wrapPem(PemLabel.PublicKey, encodeToByteArrayBlocking(EdDSA.PublicKey.Format.DER))
+            else                       -> error("$format is not supported by CryptoKit EdDSA")
+        }
+
+        override fun signatureVerifier(): SignatureVerifier = object : SignatureVerifier {
+            override fun createVerifyFunction(): VerifyFunction =
+                AccumulatingVerifyFunction { data, signature ->
+                    data.useNSData { dataNs ->
+                        signature.useNSData { sigNs ->
+                            key.verifyWithSignature(sigNs, message = dataNs)
+                        }
+                    }
+                }
+        }
+    }
+
+    private class EdPrivateKey(
+        val key: SwiftEdDsaPrivateKey,
+    ) : EdDSA.PrivateKey {
+        override fun getPublicKeyBlocking(): EdDSA.PublicKey = EdPublicKey(key.publicKey())
+
+        override fun encodeToByteArrayBlocking(format: EdDSA.PrivateKey.Format): ByteArray = when (format) {
+            EdDSA.PrivateKey.Format.RAW -> key.rawRepresentation().toByteArray()
+            EdDSA.PrivateKey.Format.DER -> wrapPrivateKeyInfo(
+                0,
+                UnknownKeyAlgorithmIdentifier(ObjectIdentifier.Ed25519),
+                key.rawRepresentation().toByteArray()
+            )
+            EdDSA.PrivateKey.Format.PEM -> wrapPem(PemLabel.PrivateKey, encodeToByteArrayBlocking(EdDSA.PrivateKey.Format.DER))
+            else                        -> error("$format is not supported by CryptoKit EdDSA")
+        }
+
+        override fun signatureGenerator(): SignatureGenerator = object : SignatureGenerator {
+            override fun createSignFunction(): SignFunction =
+                AccumulatingSignFunction { data ->
+                    swiftTry { error -> data.useNSData { key.signWithMessage(it, error) } }.toByteArray()
+                }
+
+            override fun generateSignatureBlocking(data: ByteArray): ByteArray =
+                swiftTry { error -> data.useNSData { key.signWithMessage(it, error) } }.toByteArray()
+        }
+    }
+}
---
+++ cryptography-providers/cryptokit/src/commonMain/kotlin/algorithms/CryptoKitXdh.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2025-2026 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package dev.whyoleg.cryptography.providers.cryptokit.algorithms
+
+import dev.whyoleg.cryptography.algorithms.*
+import dev.whyoleg.cryptography.materials.key.*
+import dev.whyoleg.cryptography.operations.*
+import dev.whyoleg.cryptography.providers.base.*
+import dev.whyoleg.cryptography.providers.base.materials.*
+import dev.whyoleg.cryptography.providers.cryptokit.internal.*
+import dev.whyoleg.cryptography.providers.cryptokit.internal.swift.DwcCryptoKitInterop.*
+import dev.whyoleg.cryptography.serialization.asn1.*
+import dev.whyoleg.cryptography.serialization.asn1.modules.*
+import dev.whyoleg.cryptography.serialization.pem.*
+
+internal object CryptoKitXdh : XDH {
+    override fun publicKeyDecoder(curve: XDH.Curve): KeyDecoder<XDH.PublicKey.Format, XDH.PublicKey> {
+        check(curve == XDH.Curve.X25519) { "CryptoKit supports X25519 only" }
+        return PublicKeyDecoder
+    }
+
+    override fun privateKeyDecoder(curve: XDH.Curve): KeyDecoder<XDH.PrivateKey.Format, XDH.PrivateKey> {
+        check(curve == XDH.Curve.X25519) { "CryptoKit supports X25519 only" }
+        return PrivateKeyDecoder
+    }
+
+    override fun keyPairGenerator(curve: XDH.Curve): KeyGenerator<XDH.KeyPair> {
+        check(curve == XDH.Curve.X25519) { "CryptoKit supports X25519 only" }
+        return KeyPairGenerator
+    }
+
+    private object KeyPairGenerator : KeyGenerator<XDH.KeyPair> {
+        override fun generateKeyBlocking(): XDH.KeyPair {
+            val privateKey = SwiftXdhPrivateKey.generate()
+            return XdhKeyPair(
+                publicKey = XdhPublicKey(privateKey.publicKey()),
+                privateKey = XdhPrivateKey(privateKey)
+            )
+        }
+    }
+
+    private object PublicKeyDecoder : KeyDecoder<XDH.PublicKey.Format, XDH.PublicKey> {
+        override fun decodeFromByteArrayBlocking(format: XDH.PublicKey.Format, bytes: ByteArray): XDH.PublicKey = when (format) {
+            XDH.PublicKey.Format.RAW -> XdhPublicKey(
+                swiftTry { error -> bytes.useNSData { SwiftXdhPublicKey.decodeRawWithRawRepresentation(it, error) } }
+            )
+            XDH.PublicKey.Format.DER -> {
+                val raw = unwrapSubjectPublicKeyInfo(ObjectIdentifier.X25519, bytes)
+                XdhPublicKey(swiftTry { error -> raw.useNSData { SwiftXdhPublicKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            XDH.PublicKey.Format.PEM -> {
+                val der = unwrapPem(PemLabel.PublicKey, bytes)
+                val raw = unwrapSubjectPublicKeyInfo(ObjectIdentifier.X25519, der)
+                XdhPublicKey(swiftTry { error -> raw.useNSData { SwiftXdhPublicKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            else                     -> error("$format is not supported by CryptoKit XDH")
+        }
+    }
+
+    private object PrivateKeyDecoder : KeyDecoder<XDH.PrivateKey.Format, XDH.PrivateKey> {
+        override fun decodeFromByteArrayBlocking(format: XDH.PrivateKey.Format, bytes: ByteArray): XDH.PrivateKey = when (format) {
+            XDH.PrivateKey.Format.RAW -> XdhPrivateKey(
+                swiftTry { error -> bytes.useNSData { SwiftXdhPrivateKey.decodeRawWithRawRepresentation(it, error) } }
+            )
+            XDH.PrivateKey.Format.DER -> {
+                val raw = KeyInfoUnwrap.unwrapPkcs8ForOids(bytes, listOf(ObjectIdentifier.X25519))
+                XdhPrivateKey(swiftTry { error -> raw.useNSData { SwiftXdhPrivateKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            XDH.PrivateKey.Format.PEM -> {
+                val der = unwrapPem(PemLabel.PrivateKey, bytes)
+                val raw = KeyInfoUnwrap.unwrapPkcs8ForOids(der, listOf(ObjectIdentifier.X25519))
+                XdhPrivateKey(swiftTry { error -> raw.useNSData { SwiftXdhPrivateKey.decodeRawWithRawRepresentation(it, error) } })
+            }
+            else                      -> error("$format is not supported by CryptoKit XDH")
+        }
+    }
+
+    private class XdhKeyPair(
+        override val publicKey: XDH.PublicKey,
+        override val privateKey: XDH.PrivateKey,
+    ) : XDH.KeyPair
+
+    private class XdhPublicKey(
+        val key: SwiftXdhPublicKey,
+    ) : XDH.PublicKey, SharedSecretGenerator<XDH.PrivateKey> {
+        override fun encodeToByteArrayBlocking(format: XDH.PublicKey.Format): ByteArray = when (format) {
+            XDH.PublicKey.Format.RAW -> key.rawRepresentation().toByteArray()
+            XDH.PublicKey.Format.DER -> wrapSubjectPublicKeyInfo(
+                UnknownKeyAlgorithmIdentifier(ObjectIdentifier.X25519),
+                key.rawRepresentation().toByteArray()
+            )
+            XDH.PublicKey.Format.PEM -> wrapPem(PemLabel.PublicKey, encodeToByteArrayBlocking(XDH.PublicKey.Format.DER))
+            else                     -> error("$format is not supported by CryptoKit XDH")
+        }
+
+        override fun sharedSecretGenerator(): SharedSecretGenerator<XDH.PrivateKey> = this
+        override fun generateSharedSecretToByteArrayBlocking(other: XDH.PrivateKey): ByteArray {
+            require(other is XdhPrivateKey)
+            return swiftTry { error -> other.key.deriveSecretWith(key, error) }.toByteArray()
+        }
+    }
+
+    private class XdhPrivateKey(
+        val key: SwiftXdhPrivateKey,
+    ) : XDH.PrivateKey, SharedSecretGenerator<XDH.PublicKey> {
+        override fun getPublicKeyBlocking(): XDH.PublicKey = XdhPublicKey(key.publicKey())
+
+        override fun encodeToByteArrayBlocking(format: XDH.PrivateKey.Format): ByteArray = when (format) {
+            XDH.PrivateKey.Format.RAW -> key.rawRepresentation().toByteArray()
+            XDH.PrivateKey.Format.DER -> wrapPrivateKeyInfo(
+                0,
+                UnknownKeyAlgorithmIdentifier(ObjectIdentifier.X25519),
+                key.rawRepresentation().toByteArray()
+            )
+            XDH.PrivateKey.Format.PEM -> wrapPem(PemLabel.PrivateKey, encodeToByteArrayBlocking(XDH.PrivateKey.Format.DER))
+            else                      -> error("$format is not supported by CryptoKit XDH")
+        }
+
+        override fun sharedSecretGenerator(): SharedSecretGenerator<XDH.PublicKey> = this
+        override fun generateSharedSecretToByteArrayBlocking(other: XDH.PublicKey): ByteArray {
+            require(other is XdhPublicKey)
+            return swiftTry { error -> key.deriveSecretWith(other.key, error) }.toByteArray()
+        }
+    }
+}
