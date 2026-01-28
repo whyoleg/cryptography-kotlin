13dcd252f81d1966734faa7d562634de35b090aa
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-28 08:26:26 +0200

    [JDK] WIP

--- cryptography-providers/jdk/src/jvmMain/kotlin/internal/BouncyCastleBridge.kt
+++ cryptography-providers/jdk/src/jvmMain/kotlin/internal/BouncyCastleBridge.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright (c) 2025-2026 Oleg Yukhnevich. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.whyoleg.cryptography.providers.jdk.internal
@@ -34,4 +34,48 @@ internal object BouncyCastleBridge {
         return ECPublicKeySpec(point, privateKey.params)
     }
 
+    // TODO: BC APIs, don't check for BC classes
+    // TODO: this require using jvmToolchain(21) and more implementation
+//    fun deriveEdDSAPublicKey(privateKey: JPrivateKey): JPublicKey? {
+//        if (!isAvailable) return null
+//
+//        // jdk 15 -> try/catch
+//        privateKey as? EdECPrivateKey ?: return null
+//
+//        // BC class
+//        if (privateKey is EdDSAPrivateKey) {
+//            return privateKey.publicKey
+//        }
+//
+//        // TODO: ed25519 vs ed448
+//
+//        val privateKeySeed = privateKey.bytes.getOrNull() ?: return null
+//        val encodedPublicKey = Ed25519PrivateKeyParameters(privateKeySeed, 0)
+//            .generatePublicKey()
+//            .encoded
+//
+//        val pub = priv.generatePublicKey()
+//
+//        val publicKeyBytes = pub.getEncoded()
+//
+//        return try {
+//            // BouncyCastle EdDSA private keys implement EdDSAPrivateKey interface
+//            // which provides direct access to the public key
+//            (privateKey as? EdDSAPrivateKey)?.publicKey
+//        } catch (_: Throwable) {
+//            null
+//        }
+//    }
+//
+//    fun deriveXDHPublicKey(privateKey: JPrivateKey, ): JPublicKey? {
+//        if (!isAvailable) return null
+//
+//        return try {
+//            // BouncyCastle XDH private keys implement XDHPrivateKey interface
+//            // which provides direct access to the public key
+//            (privateKey as? XDHPrivateKey)?.publicKey
+//        } catch (_: Throwable) {
+//            null
+//        }
+//    }
 }
